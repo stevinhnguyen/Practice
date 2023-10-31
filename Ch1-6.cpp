bba2911431f8726b55ea4553f50b0d48e5787c8b
Steven Nguyen <stevinhnguyen@gmail.com>
2023-10-30 17:41:22 -0700

    update

---
+++ Ch1-6.cpp
@@ -0,0 +1,39 @@
+//Write a program that prints all abundant numbers and their abundance, up to a number entered by the user.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+List Abundant(int Val)
+{
+    List Abundant;
+    for i = 12 to Val
+    {
+        List Factors = Factors(int i);
+        if (Sum(Factors) > i)
+        {
+            add i to Abundant;
+        }
+    }
+    return Abundant
+}
+
+List Factors(int Val)
+{
+    List Factors;
+    for i = 1 to Val / 2
+    {
+        if (Val % i == 0)
+        {
+            add i to Factors;
+        }
+    }
+    return Factors;
+}
+
+int main()
+{
+    
+}
+*/
